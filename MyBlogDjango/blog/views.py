d83fe336487cabd77c76b6f77a24476c2ca4fda6
David <david.morkos@global.ntt>
2020-08-14 13:25:54 +1000

    added styling and template

--- MyBlogDjango/blog/views.py
+++ MyBlogDjango/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'DavidM',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Augest 14th, 2020'
+    },
+    {
+        'author': 'Tony',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 15th, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
