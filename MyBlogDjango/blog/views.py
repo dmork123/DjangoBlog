7591fdcd88af750a32d8ea8d8fab72c1557a0a79
David <david.morkos@global.ntt>
2020-08-17 09:37:49 +1000

    small changes

--- MyBlogDjango/blog/views.py
+++ MyBlogDjango/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -7,6 +8,14 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+# class based views
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # orders the post from newwest to oldests
 
 
 def about(request):
